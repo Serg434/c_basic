6635cfae5d945d06026caf92b6dec59f165fc2e5
Sergey Zhuravlev <e252xo@gmail.com>
2021-08-08 19:13:49 +0500

    lesson_1

---
+++ lesson_1/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+  printf ("My\n\tname\n\t\tis\n\t\t\tSergey\n");
+  printf ("My\n");
+  printf ("  name\n");
+  printf ("      is\n");
+  printf ("        Sergey\n\n");
+  printf ("       *\n");
+  printf ("      ***\n");
+  printf ("     *****\n");
+  printf ("     HHHHH\n");
+  printf ("     ZZZZZ\n");
+
+  return 0;
+}
